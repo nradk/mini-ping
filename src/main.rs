eaf262ff1c2bedc72789189e194915a37b1c7481
Neeraj Adhikari <git@nradk.com>
2025-07-26 22:16:41 -0700

    Handle destination unreachable response

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,24 @@
-use std::os::fd;
 use std::env;
+use std::os::fd;
 use std::env::Args;
 use std::net::Ipv4Addr;
 use std::time::SystemTime;
 
 use nix::sys::socket;
-use nix::sys::socket::AddressFamily;
-use nix::sys::socket::SockType;
-use nix::sys::socket::SockFlag;
-use nix::sys::socket::SockProtocol;
+use nix::sys::socket::{AddressFamily, SockType, SockFlag, SockProtocol, MsgFlags};
 
 use internet_checksum::checksum;
 
-// Traits
-use std::os::fd::AsRawFd;
+use std::os::fd::AsRawFd;   // Trait
+
+const UNREACHABLE_CODE_MSGS: [&'static str; 16] = [
+    "Destination network unreachable", "Destination host unreachable", "Destination protocol unreachable",
+    "Destination port unreachable", "Fragmentation required, and DF flag set", "Source route failed",
+    "Destination network unknown", "Destination host unknown", "Source host isolated",
+    "Network administratively prohibited", "Host administratively prohibited", "Network unreachable for ToS",
+    "Host unreachable for ToS", "Communication administratively prohibited", "Host Precedence Violation",
+    "Precedence cutoff in effect"
+];
 
 fn get_address(args: &mut Args) -> Result<socket::SockaddrIn, String> {
     let executable_name = args.next().unwrap();
@@ -32,7 +37,7 @@ fn get_address(args: &mut Args) -> Result<socket::SockaddrIn, String> {
     })?;
 
     let octets = ip4address.octets();
-    return Ok(socket::SockaddrIn::new(octets[0], octets[1], octets[2], octets[3], 0));
+    Ok(socket::SockaddrIn::new(octets[0], octets[1], octets[2], octets[3], 0))
 }
 
 fn ping() -> Result<(), String> {
@@ -44,10 +49,8 @@ fn ping() -> Result<(), String> {
         SockProtocol::Icmp
     ).map_err(|e| { format!("{} - Unable to create socket!", e) })?;
 
-    let mut buf: Vec<u8> = Vec::new();
-    buf.resize(8, 0);
-    buf[0] = 0x08; // Type = 0x08 for echo request
-    buf[7] = 0x01; // Set sequence number to 1
+    // Create ICMP echo request (type 8, code 0), id 0, sequence 1
+    let mut buf: Vec<u8> = vec![0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01];
 
     // Compute and set checksum
     let checksum = checksum(buf.as_slice());
@@ -56,30 +59,26 @@ fn ping() -> Result<(), String> {
     println!("PING {}", address.ip().to_string());
     let start_time = SystemTime::now();
 
-    socket::sendto(
-        sock.as_raw_fd(),
-        buf.as_slice(),
-        &address,
-        socket::MsgFlags::empty()
-    ).map_err(|e| { format!("{} - Unable to send ICMP message!", e) })?;
+    socket::sendto(sock.as_raw_fd(), buf.as_slice(), &address, MsgFlags::empty())
+        .map_err(|e| { format!("{} - Unable to send ICMP message!", e) })?;
 
-    buf.clear();
-    buf.resize(1024, 0);
+    buf.resize(256, 0); // An arbitrary value
 
     let (recv_bytes, sender_addr): (usize, Option<socket::SockaddrIn>) =
         socket::recvfrom(sock.as_raw_fd(), buf.as_mut_slice())
         .map_err(|e| { format!("{} - Error trying to receive ICMP response!", e) })?;
+    if recv_bytes >= buf.len() {
+        panic!("Buffer full when receiving!");
+    }
 
     let end_time = SystemTime::now();
     let elapsed_duration = end_time
         .duration_since(start_time)
         .map_err(|e| format!("{} - Unable to compute elapsed time!", e))?;
 
-    if recv_bytes >= 1024 {
-        panic!("Buffer full when receiving!");
-    }
-
-    // Consume IPv4 header first
+    // For raw IP sockets, the response contains the IP header as well. The 4 LSBs of the first
+    // octet is header size as the number of 32-bit words. We extract this size and look at the
+    // rest of the message (the actual ICMP response header).
     let ip4_header_size = ((buf[0] & 0x0F) as usize) * 4;
     let response = &buf[ip4_header_size..];
 
@@ -87,14 +86,15 @@ fn ping() -> Result<(), String> {
         .map(|sock_add| sock_add.ip().to_string())
         .unwrap_or(String::from("unknown"));
 
-
-    if response[0] == 0 {
-        println!("ICMP echo response received from address {} in {:.3} ms", responder,
+    if response[0] == 0 && response[1] == 0 {
+        println!("From {}: Echo response in {:.3} ms", responder,
             elapsed_duration.as_micros() as f64 / 1000 as f64);
-        Ok(())
+    } else if response[0] == 3 && response[1] < 16 {
+        println!("From {}: Unreachable - {}", responder, UNREACHABLE_CODE_MSGS[response[1] as usize]);
     } else {
-        Err(format!("Unexpected type {} (with code {}) for ICMP response!", response[0], response[1]))
+        return Err(format!("Unexpected type {} (with code {}) for ICMP response!", response[0], response[1]))
     }
+    return Ok(())
 }
 
 fn main() {
